d226b1eaf866f6d4742106e0e16452819d95b10b
Haf1dzsr <hsramadhan68@gmail.com>
2024-10-09 17:42:31 +0700

    add: search book handler

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -6,6 +6,7 @@ import 'package:e_library/presentation/explore/cubits/query_book_cubit/query_boo
 import 'package:e_library/presentation/explore/cubits/upload_book_cubit/upload_book_cubit.dart';
 import 'package:e_library/presentation/favorite/cubits/favorite_book_cubit/favorite_book_cubit.dart';
 import 'package:e_library/presentation/onboarding/cubits/cubit/create_new_user_cubit.dart';
+import 'package:e_library/presentation/search/cubits/search_book_cubit/search_book_cubit.dart';
 import 'package:e_library/presentation/splash/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -37,6 +38,9 @@ class MyApp extends StatelessWidget {
         ),
         BlocProvider(
           create: (context) => FavoriteBookCubit(),
+        ),
+        BlocProvider(
+          create: (context) => SearchBookCubit(),
         ),
       ],
       child: MaterialApp(
--- lib/presentation/search/screens/search_screen.dart
+++ lib/presentation/search/screens/search_screen.dart
@@ -1,39 +1,72 @@
 import 'package:e_library/common/themes/app_color.dart';
 import 'package:e_library/common/themes/app_theme.dart';
+import 'package:e_library/common/widgets/custom_textformfield.dart';
 import 'package:e_library/presentation/explore/widgets/explore_book_card_widget.dart';
-import 'package:e_library/presentation/favorite/cubits/favorite_book_cubit/favorite_book_cubit.dart';
+import 'package:e_library/presentation/search/cubits/search_book_cubit/search_book_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class FavoriteScreen extends StatefulWidget {
-  const FavoriteScreen({super.key});
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
 
   @override
-  State<FavoriteScreen> createState() => _FavoriteScreenState();
+  State<SearchScreen> createState() => _SearchScreenState();
 }
 
-class _FavoriteScreenState extends State<FavoriteScreen> {
+class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController searchC = TextEditingController();
+
   @override
   void initState() {
     super.initState();
-    context.read<FavoriteBookCubit>().getFavoriteBooks();
+    context.read<SearchBookCubit>().searchBooks('');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Favorite Books',
-          style: appTheme.textTheme.headlineMedium!
-              .copyWith(color: AppColor.textPrimary),
+      appBar: PreferredSize(
+        preferredSize:
+            Size.fromHeight(MediaQuery.sizeOf(context).height * 0.1125),
+        child: AppBar(
+          title: Text(
+            'Search Books',
+            style: appTheme.textTheme.headlineMedium!
+                .copyWith(color: AppColor.textPrimary),
+          ),
+          bottom: PreferredSize(
+            preferredSize:
+                Size.fromHeight(MediaQuery.sizeOf(context).height * 0.1),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16, bottom: 8),
+              child: CustomTextFormField(
+                textInputAction: TextInputAction.done,
+                hint: 'Search Books',
+                controller: searchC,
+                prefixIcon: const Icon(
+                  Icons.search_rounded,
+                  color: AppColor.grey,
+                ),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    searchC.clear();
+                    context.read<SearchBookCubit>().searchBooks('');
+                  },
+                  icon: const Icon(Icons.clear_rounded),
+                ),
+                onChanged: (query) {
+                  context.read<SearchBookCubit>().searchBooks(query);
+                },
+              ),
+            ),
+          ),
+          backgroundColor: AppColor.white,
+          elevation: 0,
+          automaticallyImplyLeading: false,
         ),
-        backgroundColor: AppColor.white,
-        elevation: 0,
-        automaticallyImplyLeading: false,
       ),
       body: SafeArea(
-        child: BlocBuilder<FavoriteBookCubit, FavoriteBookState>(
+        child: BlocBuilder<SearchBookCubit, SearchBookState>(
           builder: (context, state) {
             return state.maybeWhen(
               error: (message) {
@@ -58,7 +91,7 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
                 return favoriteBooks.isEmpty
                     ? Center(
                         child: Text(
-                          'No favorite books yet',
+                          'No books found',
                           style: appTheme.textTheme.bodyMedium!
                               .copyWith(color: AppColor.textSecondary),
                         ),
@@ -91,7 +124,7 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
               orElse: () {
                 return Center(
                   child: Text(
-                    'Failed to get favorited books',
+                    'Failed to get books',
                     style: appTheme.textTheme.bodyMedium!
                         .copyWith(color: AppColor.textSecondary),
                   ),
